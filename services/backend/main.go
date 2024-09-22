8b5078a30b03e16455fe6c1424fe054726f8695a
Andy Autida <risejandiautida@gmail.com>
2024-09-23 02:03:51 +0800

    add embedded index template

--- services/backend/main.go
+++ services/backend/main.go
@@ -8,12 +8,20 @@ import (
 )
 
 func main() {
-	mux := http.NewServeMux()
-	store := ds.CreateUserDatastore([]ds.User{
+	users := []ds.User{
 		{ID: "1", Name: "bob"},
-	})
+	}
+	store := ds.CreateUserDatastore(users)
+
+	indexH, err := handler.NewIndexHandler(store)
+	if err != nil {
+		panic(err)
+	}
 	usersH := handler.NewUsersHandler(store)
 	userH := handler.NewUserHandler(store)
+
+	mux := http.NewServeMux()
+	mux.Handle("/", indexH)
 	mux.Handle("/users/", usersH)
 	mux.Handle("/users/{id}", userH)
 	http.ListenAndServe(":1337", mux)
